1a8de1398a624a66aa4fb31aae2abb37e4b2ae6b
elivingt <elivingt@gmail.com>
2020-01-26 19:05:24 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,3 @@
-/*
- *
- * Copyright (c) 2015. Nexar Inc. - All Rights Reserved. Proprietary and confidential.
- *
- * Unauthorized copying of this file, via any medium is strictly prohibited.
- *
- */
 
 import java.util.List;
 import java.util.Set;
