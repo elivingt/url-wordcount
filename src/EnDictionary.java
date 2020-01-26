aa556d6a25334e10175f34ab46aff25fc486b8cc
elivingt <elivingt@gmail.com>
2020-01-26 19:04:43 +0200

    Update EnDictionary.java

--- src/EnDictionary.java
+++ src/EnDictionary.java
@@ -1,10 +1,3 @@
-/*
-*
-* Copyright (c) 2015. Nexar Inc. - All Rights Reserved. Proprietary and confidential.
-*
-* Unauthorized copying of this file, via any medium is strictly prohibited.
-*
-*/
 
 import java.io.FileOutputStream;
 import java.io.IOException;
